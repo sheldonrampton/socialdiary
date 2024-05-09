853df4d91bca94ea3036e8b303b16b932d03d688
sheldonrampton <sheldonmrampton@gmail.com>
2024-05-09 17:56:37 -0500

    add a bit more custom tag handling

--- cleanup_html.py
+++ cleanup_html.py
@@ -6,11 +6,16 @@ def strip_html(html_content):
     # Use BeautifulSoup to parse the HTML content
     soup = BeautifulSoup(html_content, 'lxml')
     
+    # Remove custom tags and their contents if needed
+    for tag in soup.find_all(['x-stuff-for-pete', 'x-sigsep']):
+        tag.decompose()
+    
     # Extract text from the parsed HTML
     text = soup.get_text(separator='\n', strip=True)
     return text
 
 # Load the HTML content from a file
+# with open('gmail/samples/message_16880adbfd9978a8.txt', 'r') as file:
 with open('gmail/samples/untitled 3.html', 'r') as file:
     html_content = file.read()
 
